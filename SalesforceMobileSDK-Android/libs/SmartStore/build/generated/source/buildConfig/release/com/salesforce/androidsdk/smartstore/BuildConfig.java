973401d4b192adc7c457e655bc92688347c39fda
geowan <geoffreymwangi5378@gmail.com>
2020-09-10 18:09:28 +0300

    first commit

---
+++ SalesforceMobileSDK-Android/libs/SmartStore/build/generated/source/buildConfig/release/com/salesforce/androidsdk/smartstore/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.salesforce.androidsdk.smartstore;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.salesforce.androidsdk.smartstore";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 69;
+  public static final String VERSION_NAME = "8.2.0";
+}
